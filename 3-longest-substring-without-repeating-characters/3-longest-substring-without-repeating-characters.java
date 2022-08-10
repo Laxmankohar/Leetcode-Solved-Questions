00fef0d3d47fc15414b563acf5b2a2891ab7eb4c
Laxman kohar <73365025+Laxmankohar@users.noreply.github.com>
2022-08-10 19:58:57 +0530

    Time: 12 ms (58.86%), Space: 45.9 MB (37.27%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -5,10 +5,14 @@ class Solution {
         int i = 0, j= 0, max = 0;
         
         while(j < s.length()){
+            
+//             if character is not present, we add it to set and find max
             if(!set.contains(s.charAt(j))){
                 set.add(s.charAt(j++));
                 max = Math.max(max, set.size());
             }
+            
+//             if character is repeating , we remove from the set
             else{
                 set.remove(s.charAt(i++));
             }
