6d040c47e3af88f9a41df5be08d371697b978f86
Laxman kohar <73365025+Laxmankohar@users.noreply.github.com>
2022-08-15 12:09:04 +0530

    Time: 5 ms (71.63%), Space: 44 MB (64.21%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,19 +1,40 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-         HashMap<Integer,Integer> hs = new HashMap();
-        Stack<Integer> stack = new Stack();
+        // int ans [] =  new int[nums1.length];
+        Stack<Integer> st = new Stack<>();
         
-        for(Integer num : nums2){
-            while(!stack.isEmpty() && stack.peek() < num){
-                hs.put(stack.pop(), num);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : nums2){
+            while(!st.isEmpty() && st.peek() < i){
+                map.put(st.pop(), i);
             }
-            stack.push(num);
+            st.push(i);
         }
         
-        for(int i=0;i<nums1.length;i++){
-            nums1[i] = hs.getOrDefault(nums1[i],-1);
+        int i =0;
+        
+        for(;i<nums1.length;i++){
+            nums1[i ]= map.getOrDefault(nums1[i], -1);
         }
         return nums1;
+        
+        
+        
+//         HashMap<Integer,Integer> hs = new HashMap();
+//         Stack<Integer> stack = new Stack();
+        
+//         for(Integer num : nums2){
+//             while(!stack.isEmpty() && stack.peek() < num){
+//                 hs.put(stack.pop(), num);
+//             }
+//             stack.push(num);
+//         }
+        
+//         for(int i=0;i<nums1.length;i++){
+//             nums1[i] = hs.getOrDefault(nums1[i],-1);
+//         }
+//         return nums1;
     }
 }
