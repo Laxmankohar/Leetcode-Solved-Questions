94f5dfc9e2dbb97e5ef44b9d93763650ff11ad27
Laxman kohar <73365025+Laxmankohar@users.noreply.github.com>
2022-09-26 00:44:12 +0530

    Time: 0 ms (100.00%), Space: 71.9 MB (70.84%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0;
+        int r = arr.length-1;
+        
+        while(l < r){
+            int mid = (l+r)/2;
+            
+            if(arr[mid] > arr[mid+1]){
+                r = mid;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+}
