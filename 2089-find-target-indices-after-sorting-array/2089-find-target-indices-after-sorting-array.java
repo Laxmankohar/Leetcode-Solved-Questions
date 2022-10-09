16b1491499d43320ac17f0016721bdd776399a6d
Laxman kohar <73365025+Laxmankohar@users.noreply.github.com>
2022-10-09 20:53:27 +0530

    Time: 4 ms (58.96%), Space: 44 MB (28.47%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -4,11 +4,39 @@ class Solution {
         
         Arrays.sort(nums);
         
-        for(int i = 0 ; i < nums.length; i++){
-            if(nums[i] == target){
-                list.add(i);
+        // for(int i = 0 ; i < nums.length; i++){
+        //     if(nums[i] == target){
+        //         list.add(i);
+        //     }
+        // }
+        // return list;
+        
+        
+        
+        int start = 0;
+        int end = nums.length -1 ;
+        
+        while(start < end){
+            int mid = (start + end)/2;
+             if(nums[mid] >= target){
+                end = mid;
+            }
+            else{
+                start = mid+1;
             }
         }
+        
+        while(nums[start] == target){
+            list.add(start);
+            
+            if(start != nums.length-1){
+                start++;
+            }
+            else{
+                break;
+            }
+        }
+        
         return list;
     }
 }
