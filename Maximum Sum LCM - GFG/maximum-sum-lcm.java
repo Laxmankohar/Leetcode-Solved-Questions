dcb25f1737e9ce407746fee48e15a7c298384349
Laxman kohar <73365025+Laxmankohar@users.noreply.github.com>
2022-11-23 09:46:07 +0530

    Added solution - LeetHub

---
+++ Maximum Sum LCM - GFG/maximum-sum-lcm.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.maxSumLCM(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static long maxSumLCM(int n) {
+        long maxSum = 0;
+   for (int i = 1; i*i <= n; i++){
+      if (n % i == 0) {
+         if (i == (n/i))
+            maxSum = maxSum + i;
+         else
+            maxSum = maxSum + i + (n/i);
+      }
+   }
+   return maxSum;
+    }
+}
