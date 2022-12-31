1a1ae8b79e1d916b205a82c4d3aba7937c9b918e
Laxman kohar <73365025+Laxmankohar@users.noreply.github.com>
2022-12-31 20:45:37 +0545

    Time: 0 ms (100.00%), Space: 41.4 MB (85.33%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+        
+//         if(n ==1){
+//             return 1;
+//         }
+        
+//         return myPow(x*n, n-1);
+    }
+}
